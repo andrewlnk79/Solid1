005ec7ea71c587c04f7b943c80b641d623833631
Andrey Linnik <av.linnik79@ya.ru>
2023-07-23 11:17:19 +0300

    added RegularAccount.cs

---
+++ Solid1/RegularAccount.cs
@@ -0,0 +1,21 @@
+﻿namespace Solid1;
+
+public class RegularAccount : Account
+{
+    public RegularAccount(string type, double balance, double interest) : base(type, balance, interest)
+    {
+    }
+    void CalculateInterests()
+    {
+        if (Balance < 1000)
+        {
+            Interest -= Balance * 0.2;
+        }
+
+        else
+        {
+            Interest -= Balance * 0.4;
+
+        }
+    }
+}
--- Solid1/SalaryAccount.cs
+++ Solid1/SalaryAccount.cs
@@ -4,12 +4,12 @@ public class SalaryAccount : Account
 {
     public SalaryAccount(string type, double balance, double interest) : base(type, balance, interest)
     {
-        
+
     }
 
     public void CalculateInterests()
     {
-        Balance -= Interest * 0.5;
+        Interest = Interest * 0.5;
     }
 }
 
